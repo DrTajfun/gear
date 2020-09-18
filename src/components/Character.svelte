5e48032b4675ed73848c65464651d0ae3c7b597b
DrTajfun <drtajfun@gmail.com>
2020-09-18 15:01:31 +0200

    a7

--- src/components/Character.svelte
+++ src/components/Character.svelte
@@ -1,16 +1,14 @@
 
-<script context="module">
+<script>
 export async function preload(page, session) {
 		const { slug } = page.params;
 
-		const res = await this.fetch('https://swgoh.gg/api/characters/1', {mode: 'no-cors',  dataType:'jsonp'});
+		const res = await this.fetch('https://swgoh.gg/api/characters/1', {mode: 'no-cors',  dataType:'json'});
 		const article = await res.json();
         console.log(article);
 		return { article };
 }
-</script>
 
-<script>
 export let article;
 </script>
 
